4b7293a5bd6a4cbfdb6af90ce2f9fb261774dff4
ronakpatel <ronakrajnipatel@gmail.com>
2024-10-26 17:12:19 -0400

    Drop Shadowed Navbar

--- gamebacklog/components/Navbar.tsx
+++ gamebacklog/components/Navbar.tsx
@@ -1,6 +1,9 @@
 export default function Navbar() {
     return (
-      <div className=" w-screen h-24 bg-sky-50">
+      <div className=" w-screen h-28 bg-sky-50 drop-shadow-2xl">
+        <div className="bg-red-500 w-1/12 h-28 place-items-center">
+          test
+        </div>
       </div>
     )
   }
