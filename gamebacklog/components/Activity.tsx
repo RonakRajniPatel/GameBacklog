30147161252579232aad866eb58145e00d2c3b76
ronakpatel <ronakrajnipatel@gmail.com>
2024-10-30 10:21:34 -0400

    Centered activity and formatted header

--- gamebacklog/components/Activity.tsx
+++ gamebacklog/components/Activity.tsx
@@ -13,34 +13,37 @@ export function Activity() {
   return (
 
     // Carousel Row
-    <div className="w-full ml-28">
-      <header className="text-4xl font-bold">
+    <div className="w-full">
+      <header className="text-4xl font-bold ml-28">
         Recent Activity
       </header>
-      <Carousel
-        opts={{
-          align: "start",
-          loop: true
-        }}
-        className="w-full max-w-sm"
-      >
+      <div className="justify-center flex">
         
-        <CarouselContent className="">
-          {Array.from({ length: 12 }).map((_, index) => (
-            <CarouselItem key={index} className="md:basis-1/3 lg:basis-1/6">
-              <div className="p-1">
-                <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-3xl">{index + 1}</span>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+        <Carousel
+          opts={{
+            align: "start",
+            loop: true
+          }}
+          className="w-full max-w-sm"
+        >
+          
+          <CarouselContent className="">
+            {Array.from({ length: 12 }).map((_, index) => (
+              <CarouselItem key={index} className="md:basis-1/3 lg:basis-1/6">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-3xl">{index + 1}</span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </div>
   )
 }
