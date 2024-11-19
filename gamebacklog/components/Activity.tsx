3889b868dc55e73dca0c5c5bddd4ebcdcc78250c
ronakpatel <ronakrajnipatel@gmail.com>
2024-11-18 21:05:19 -0500

    Card worked on

--- gamebacklog/components/Activity.tsx
+++ gamebacklog/components/Activity.tsx
@@ -1,6 +1,14 @@
 import * as React from "react"
 
-import { Card, CardContent } from "@/components/ui/card"
+import Image from "next/image"
+
+
+import { 
+  Card, 
+  CardContent,
+  CardFooter
+} from "@/components/ui/card"
+
 import {
   Carousel,
   CarouselContent,
@@ -10,40 +18,51 @@ import {
 } from "@/components/ui/carousel"
 
 export function Activity() {
-  return (
+  const items = Array(12).fill({
+    title: "The Legend of Zelda Breath of the Wild",
+    image: "/placeholder.svg?height=300&width=200",
+  })
 
-    // Carousel Row
+  return (
+  <>
+     {/* Carousel Row */}
     <div className="w-full">
       <header className="text-4xl font-bold ml-28">
         Recent Activity
       </header>
-      <div className="justify-center flex">
-        
-        <Carousel
-          opts={{
-            align: "start",
-            loop: true
-          }}
-          className="w-full max-w-sm"
-        >
-          
-          <CarouselContent className="">
-            {Array.from({ length: 12 }).map((_, index) => (
-              <CarouselItem key={index} className="md:basis-1/3 lg:basis-1/6">
-                <div className="p-1">
-                  <Card>
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <span className="text-3xl">{index + 1}</span>
-                    </CardContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-      </div>
     </div>
+<div className="w-full max-w-6xl mx-auto px-4">
+  <Carousel
+    opts={{
+      align: "start",
+      loop: true,
+    }}
+    className="w-full"
+  >
+    <CarouselContent className="-ml-2 md:-ml-4">
+      {items.map((item, index) => (
+        <CarouselItem key={index} className="pl-2 md:pl-4 md:basis-1/3 lg:basis-1/6">
+            <Card className="border-none shadow-lg rounded-lg overflow-hidden drop-shadow-xl bg-slate-100">
+              <CardContent className="px-2.5 pt-2.5 pb-1">
+                <Image
+                  src={"/images/gameCover.jpg"}
+                  width={1000}
+                  height={2000}
+                  alt={item.title}
+                  className="object-cover w-full h-full rounded"
+                />
+                </CardContent>
+                <CardFooter className="pb-1">
+                  <h2 className="text-sm font-heavy leading-1"><b>{item.title}</b></h2>
+                </CardFooter>
+            </Card>
+        </CarouselItem>
+      ))}
+    </CarouselContent>
+    <CarouselPrevious/>
+    <CarouselNext/>
+  </Carousel>
+  </div>
+</>
   )
 }
