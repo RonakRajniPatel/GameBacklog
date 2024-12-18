d8361751f9e3b0f6b4e58c725d63edfb49630fe6
ronakpatel <ronakrajnipatel@gmail.com>
2024-12-18 16:24:51 -0500

    Fixed sizing issue and its now consistent across screens. I had hard set 96 somewhere before, but fixed it now.

--- gamebacklog/app/page.tsx
+++ gamebacklog/app/page.tsx
@@ -3,7 +3,7 @@ import { Activity } from "@/components/Activity";
 export default function Home() {
   return (
     <div>
-      <div className="pb-32">
+      <div className="">
         <Activity/>
       </div>
       <Activity/>
--- gamebacklog/components/Activity.tsx
+++ gamebacklog/components/Activity.tsx
@@ -31,46 +31,46 @@ export function Activity() {
         Recent Activity
       </header>
     </div>
-<div className="w-full max-w-fit mx-auto px-56 h-96">
-  <Carousel
-    opts={{
-      align: "start",
-      loop: true,
-    }}
-    className="w-full h-full"
-  >
-    <CarouselContent className="-ml-2 md:-ml-4">
-      {items.map((item, index) => (
-        <CarouselItem key={index} className="pl-2 md:pl-4 md:basis-1/3 lg:basis-1/6">
-            <Card className="border-none shadow-lg rounded-lg overflow-hidden drop-shadow-xl bg-slate-100 m-1.5">
-              <CardContent className="px-2.5 pt-2.5 pb-1">
-                <div className="group relative">
-                  <Image
-                    src={"/images/gameCover.jpg"}
-                    width={1000}
-                    height={2000}
-                    alt={item.title}
-                    className="object-cover rounded"
-                  />
-                  <div className="hidden group-hover:inline absolute top-0 right-0 w-2/5 h-full object-contain">
-                    <div className="bg-purple-500 h-1/4 opacity-85">Played</div>
-                    <div className="bg-red-500 h-1/4 opacity-85">Drop</div>
-                    <div className="bg-green-500 h-1/4 opacity-85">Want</div>
-                    <div className="bg-blue-500 h-1/4 opacity-85">Still</div>
-                  </div>
-                </div>
-                </CardContent>
-                <CardFooter className="pb-1 text-left">
-                  <h2 className="text-md text-base/4 font-semibold">{item.title}</h2>
-                </CardFooter>
-            </Card>
-        </CarouselItem>
-      ))}
-    </CarouselContent>
-    <CarouselPrevious className="top-2/3"/>
-    <CarouselNext className="top-2/3"/>
-  </Carousel>
-  </div>
+    <div className="w-full max-w-fit mx-auto px-56">
+      <Carousel
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+        className="w-full h-full "
+      >
+        <CarouselContent className="">
+          {items.map((item, index) => (
+            <CarouselItem key={index} className="pl-2 md:pl-4 md:basis-1/3 lg:basis-1/6 xl:basis-1/6 2xl:basis-1/12">
+                <Card className="border-none shadow-lg rounded-lg overflow-hidden drop-shadow-xl bg-slate-100 m-1.5">
+                  <CardContent className="px-2.5 pt-2.5 pb-1">
+                    <div className="group relative">
+                      <Image
+                        src={"/images/gameCover.jpg"}
+                        width={500}
+                        height={1000}
+                        alt={item.title}
+                        className="object-cover rounded"
+                      />
+                      <div className="hidden group-hover:inline absolute top-0 right-0 w-2/5 h-full object-contain">
+                        <div className="bg-purple-500 h-1/4 opacity-85">Played</div>
+                        <div className="bg-red-500 h-1/4 opacity-85">Drop</div>
+                        <div className="bg-green-500 h-1/4 opacity-85">Want</div>
+                        <div className="bg-blue-500 h-1/4 opacity-85">Still</div>
+                      </div>
+                    </div>
+                    </CardContent>
+                    <CardFooter className="pb-1 text-left">
+                      <h2 className="text-md text-base/4 font-semibold">{item.title}</h2>
+                    </CardFooter>
+                </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="top-2/3"/>
+        <CarouselNext className="top-2/3"/>
+      </Carousel>
+    </div>
 </>
   )
 }
