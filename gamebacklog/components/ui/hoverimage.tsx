cc0a13893688138c4e232ec7a2b288dca2a73aff
ronakpatel <ronakrajnipatel@gmail.com>
2024-12-27 13:37:45 -0500

    added style prop

--- gamebacklog/components/ui/hoverimage.tsx
+++ gamebacklog/components/ui/hoverimage.tsx
@@ -9,12 +9,15 @@ interface HoverImageProps {
 }
 
 const HoverImage: React.FC<HoverImageProps> = ({ src, alt, style }) => {
+  if (style == "cover") {
+    const hoverWidth = "w-2/5";
+  }
   return (
     <div className="group relative">
       <Image src={src} alt={alt} width={1920} height={1080} className="" />
       <div className="hidden md:block">
         <div className="hidden md:block">
-          <div className="hidden group-hover:inline absolute top-0 right-0 w-2/5 h-full object-contain">
+          <div className="hidden group-hover:inline absolute top-0 right-0 hoverWidth h-full object-contain">
             <div className="bg-purple-500 h-1/4 opacity-85">Played</div>
             <div className="bg-red-500 h-1/4 opacity-85">Drop</div>
             <div className="bg-green-500 h-1/4 opacity-85">Want</div>
