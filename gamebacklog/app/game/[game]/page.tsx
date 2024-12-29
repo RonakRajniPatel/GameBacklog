f7f661a0aea671c1cda2bb69f824d59b933b8a08
ronakpatel <ronakrajnipatel@gmail.com>
2024-12-29 18:47:07 -0500

    Game Base Layout [Needs Responsive Review Component]

--- gamebacklog/app/game/[game]/page.tsx
+++ gamebacklog/app/game/[game]/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 import Image from "next/image";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { useState, useRef } from "react";
 
 export default function Home() {
@@ -17,64 +23,86 @@ export default function Home() {
   };
 
   const formattedDate = game.date_released.toISOString().split("T")[0];
-
   return (
-    <div className="flex justify-center min-h-screen">
-      <div className="flex flex-col items-center w-full px-2 2xl:max-w-screen-2xl bg-red-500">
-        <h1 className="text-xl font-bold">{game.title}</h1>
-        <div className="object-scale-down size-3/4 md:w-1/12">
-          <Image
-            src={"/images/gameCover.jpg"}
-            width={1000}
-            height={1363}
-            alt={game.title}
-            className="object-cover rounded-2xl"
-          />
-        </div>
-        <div className="flex flex-col w-2/3 pb-2 font-thin text-lg leading-tight bg-purple-500">
-          <h2 className="font-normal text-xl text-center">
-            {"Score: "}
-            {game.score}
-          </h2>
-        </div>
-        <div className="flex flex-col w-2/3 pb-2 font-thin text-lg leading-tight bg-green-500">
-          <h2 className="font-normal text-xl text-center">
-            <u>Information</u>
-          </h2>
-          <h3>
-            {"Publisher: "} {game.publisher}
-          </h3>
-          <h3>
-            {"Main Developers: "}
-            {game.developers[0]}
-          </h3>
-          <h3>
-            Genres:
-            {game.genres.map((genre, index) => (
-              <span className="rounded-lg bg-gray-200 text-b" key={index}>
-                {genre}
-                {index < game.genres.length - 1 && ", "}
-              </span>
-            ))}
-          </h3>
-          <h3>Date Rleased: {formattedDate}</h3>
-          <h3>Platform: {game.platform}</h3>
-        </div>
-        <div className="flex flex-col w-2/3 pb-2 bg-blue-500 text-lg font-thin">
-          <h2 className="font-normal text-xl">
-            <u>Time to Beat</u>
-          </h2>
-          <h3>
-            {"Normal: "}
-            {game.est_playtime[0]} hours
-          </h3>
-          <h3>
-            {"Completionist: "}
-            {game.est_playtime[1]} hours
-          </h3>
-        </div>
-        <div className="bg-yellow-500 pb-2 w-2/3 flex flex-col">
-          <h2 className="font-normal text-lg">{game.story}</h2>
+    <div className="flex min-h-screen justify-center">
+      <div
+        className={`flex w-full flex-col items-center bg-red-500 px-2 md:gap-10 2xl:max-w-screen-2xl`}
+      >
+        <div className="md:flex md:flex-row md:gap-x-10">
+          <div className="bg-pink-500 md:w-8/12">
+            <div className="bg-purple-500 text-start text-xl font-bold md:flex md:h-1/6 md:flex-row md:place-items-center">
+              <h1 className="bg-blue-300 text-2xl md:w-9/12 md:p-5 md:text-4xl">
+                {game.title}
+              </h1>
+              <h1 className="bg-green-300 text-lg md:w-3/12 md:p-3 md:text-right md:text-4xl">
+                Score: {game.score}
+              </h1>
+            </div>
+
+            <h2 className="hidden bg-yellow-500 p-5 text-lg font-normal md:block">
+              {game.story}
+            </h2>
+          </div>
+          <div className="flex flex-col rounded-2xl px-10 md:order-first md:ml-5 md:w-3/12 md:bg-gray-200 md:p-5 md:pt-5">
+            <div className="object-scale-down">
+              <Image
+                src={"/images/gameCover.jpg"}
+                width={1000}
+                height={1363}
+                alt={game.title}
+                className="rounded-2xl object-cover"
+              />
+            </div>
+            <div className="flex flex-col bg-green-500 pb-2 text-lg font-thin leading-tight">
+              <h2 className="text-xl font-normal">
+                <u>Information</u>
+              </h2>
+              <h3>
+                {"Publisher: "} {game.publisher}
+              </h3>
+              <h3>
+                {"Main Developers: "}
+                {game.developers[0]}
+              </h3>
+              <h3>
+                Genres:
+                {game.genres.map((genre, index) => (
+                  <span className="text-b rounded-lg bg-gray-200" key={index}>
+                    {genre}
+                    {index < game.genres.length - 1 && ", "}
+                  </span>
+                ))}
+              </h3>
+              <h3>Date Rleased: {formattedDate}</h3>
+              <h3>Platform: {game.platform}</h3>
+            </div>
+            <div className="flex flex-col bg-blue-500 pb-2 text-lg font-thin">
+              <h2 className="text-xl font-normal">
+                <u>Time to Beat</u>
+              </h2>
+              <h3>
+                {"Normal: "}
+                {game.est_playtime[0]} hours
+              </h3>
+              <h3>
+                {"Completionist: "}
+                {game.est_playtime[1]} hours
+              </h3>
+            </div>
+
+            <Accordion
+              type="single"
+              collapsible
+              className="flex w-full flex-col bg-yellow-500 pb-2 md:hidden"
+            >
+              <AccordionItem value="item-1">
+                <AccordionTrigger className="text-xl">Story</AccordionTrigger>
+                <AccordionContent className="text-lg font-normal">
+                  {game.story}
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
         </div>
       </div>
     </div>
