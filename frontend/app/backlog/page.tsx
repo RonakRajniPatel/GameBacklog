773e026f9c0a5bb17c3c4a545dab265dd7805b77
ronakpatel <ronakrajnipatel@gmail.com>
2025-02-16 15:15:15 -0500

    review added on front end api

--- frontend/app/backlog/page.tsx
+++ frontend/app/backlog/page.tsx
@@ -1,10 +1,17 @@
 import "./../styles.css";
 import { TrackedGame } from "@/components/TrackedGame";
 
+const data = await fetch("http://localhost:8080/review/reviews", {
+  cache: "no-store",
+});
+const games = await data.json();
+console.log("data will be shown below");
+console.log(games);
+
 export default function Home() {
   return (
-    <div className="flex justify-center min-h-screen">
-      <div className="flex flex-col items-center w-full px-4 2xl:max-w-screen-2xl">
+    <div className="flex min-h-screen justify-center">
+      <div className="flex w-full flex-col items-center px-4 2xl:max-w-screen-2xl">
         <TrackedGame />
         <TrackedGame />
       </div>
