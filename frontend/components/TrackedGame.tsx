d446b920408aaa788ec0c64ba6b830d72925af95
ronakpatel <ronakrajnipatel@gmail.com>
2025-02-18 21:57:02 -0500

    Review propped

--- frontend/components/TrackedGame.tsx
+++ frontend/components/TrackedGame.tsx
@@ -5,7 +5,10 @@ import Image from "next/image";
 import HoverImage from "./ui/hoverimage";
 import { Button } from "./ui/button";
 
-export function TrackedGame() {
+export function TrackedGame(props: any) {
+  const { review } = props;
+  console.log("Tracked Game");
+  console.log(review);
   const game = {
     title: "The Legend of Zelda: Breath of the Wild",
     platform: "Nintendo Switch",
@@ -28,15 +31,15 @@ export function TrackedGame() {
     : game.review.slice(0, overflowLength);
   return (
     <div
-      className={`bg-gray-200 rounded-lg w-full flex flex-row h-auto max-h-44 md:max-h-96 pb-4 md:pb-0`}
+      className={`flex h-auto max-h-44 w-full flex-row rounded-lg bg-gray-200 pb-4 md:max-h-96 md:pb-0`}
     >
-      <div className={`flex flex-row w-3/12 md:w-1/6 h-full bg-yellow-500`}>
+      <div className={`flex h-full w-3/12 flex-row bg-yellow-500 md:w-1/6`}>
         <Image
           src={"/images/gameCover.jpg"}
           width={1000}
           height={1363}
           alt={game.title}
-          className="rounded p-3 pb-0 md:hidden object-scale-down"
+          className="rounded object-scale-down p-3 pb-0 md:hidden"
         />
         <div className="p-6 pb-0">
           <HoverImage
@@ -46,29 +49,29 @@ export function TrackedGame() {
           />
         </div>
       </div>
-      <div className="flex flex-col w-9/12 md:w-5/6 h-full p-4 bg-red-500">
-        <div className="flex flex-col w-full h-full md:hidden bg-blue-500">
-          <h1 className="flex font-semibold text-xl overflow-hidden">
+      <div className="flex h-full w-9/12 flex-col bg-red-500 p-4 md:w-5/6">
+        <div className="flex h-full w-full flex-col bg-blue-500 md:hidden">
+          <h1 className="flex overflow-hidden text-xl font-semibold">
             {game.title}
           </h1>
-          <div className="flex flex-row pt-2 font-thin text-sm leading-tight justify-between bg-purple-500 w-full mt-auto">
-            <h3 className="flex flex-row font-semibold text-md leading-tight bg-green-500">
+          <div className="mt-auto flex w-full flex-row justify-between bg-purple-500 pt-2 text-sm font-thin leading-tight">
+            <h3 className="text-md flex flex-row bg-green-500 font-semibold leading-tight">
               Rating: {game.ratings}
             </h3>
-            <h3 className="flex flex-row font-semibold text-md leading-tight bg-pink-500">
+            <h3 className="text-md flex flex-row bg-pink-500 font-semibold leading-tight">
               Status: {game.status}
             </h3>
           </div>
         </div>
         <div
-          className={`hidden  ${isExpanded ? "h-fit" : "h-2/5"} md:flex flex-row bg-blue-500`}
+          className={`hidden ${isExpanded ? "h-fit" : "h-2/5"} flex-row bg-blue-500 md:flex`}
         >
-          <div className=" w-7/12 overflow-hidden">
-            <h1 className="flex flex-col font-medium text-4xl pr-20">
+          <div className="w-7/12 overflow-hidden">
+            <h1 className="flex flex-col pr-20 text-4xl font-medium">
               {game.title}
             </h1>
           </div>
-          <div className="flex flex-col w-5/12 pt-2 font-thin text-lg leading-tight">
+          <div className="flex w-5/12 flex-col pt-2 text-lg font-thin leading-tight">
             <h3>Status: {game.status}</h3>
             <h3>Hours Played: {game.hours}</h3>
             <h3>Ratings: {game.ratings}</h3>
@@ -76,14 +79,14 @@ export function TrackedGame() {
             <h3>Platform: {game.platform}</h3>
           </div>
         </div>
-        <div className="hidden md:block w-full h-3/5 pt-5 font-normal text-lg object-top overflow-hidden">
+        <div className="hidden h-3/5 w-full overflow-hidden object-top pt-5 text-lg font-normal md:block">
           <h3 className="bg-purple-500">
             {shown_review}
             {game.review.length < overflowLength || isExpanded ? "" : "..."}
             <Button
               variant="link"
               onClick={toggleExpansion}
-              className={`font-normal text-lg ${game.review.length < overflowLength ? "hidden" : ""}`}
+              className={`text-lg font-normal ${game.review.length < overflowLength ? "hidden" : ""}`}
             >
               {isExpanded ? "Show Less" : "Show More"}
             </Button>
